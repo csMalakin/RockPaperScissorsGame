e81974004ba0326da9b1b463ff43420eceed3cbc
Malakin <cs-malakin@gmail.com>
2022-05-27 11:48:32 +0200

    Not pretty but rare

--- assets/main.js
+++ assets/main.js
@@ -4,10 +4,16 @@ const bestOf3=document.querySelector('#bestOf3')
 const bestOf5=document.querySelector('#bestOf5')
 const bestOf7=document.querySelector('#bestOf7')
 const chosenBestOf=document.querySelector('#bestOf')
-
+// eventListener
 bestOf3.addEventListener('click', set3Active)
 bestOf5.addEventListener('click', set5Active)
 bestOf7.addEventListener('click', set7Active)
+
+document.querySelector('#bestOf3Again').addEventListener('click',again3)
+
+document.querySelector('#bestOf5Again').addEventListener('click',again5)
+
+document.querySelector('#bestOf7Again').addEventListener('click',again7)
 
 let gameTyp=0;
 
@@ -18,7 +24,11 @@ function set3Active() {
     bestOf3.classList.add('active')
     chosenBestOf.innerText = 'We are playing a best of 3'
     gameTyp=3;
+ 
 
+    document.querySelector('#selectVariantFirst').classList.add('hidden')
+    document.querySelector('#chooseHand').classList.remove('hidden')
+    document.querySelector('#bestOfSelection').classList.add('hidden')
 }
 
 function set5Active() {
@@ -28,6 +38,12 @@ function set5Active() {
     bestOf5.classList.add('active')
     chosenBestOf.innerText = 'We are playing a best of 5'
     gameTyp=5;
+
+    document.querySelector('#selectVariantFirst').classList.add('hidden')
+    document.querySelector('#chooseHand').classList.remove('hidden')
+
+    document.querySelector('#bestOfSelection').classList.add('hidden')
+  
 }
 
 function set7Active() {
@@ -37,11 +53,68 @@ function set7Active() {
     bestOf7.classList.add('active')
     chosenBestOf.innerText = 'We are playing a best of 7'
     gameTyp=7;
+
+
+    document.querySelector('#selectVariantFirst').classList.add('hidden')
+    document.querySelector('#chooseHand').classList.remove('hidden')
+   
+    document.querySelector('#bestOfSelection').classList.add('hidden')
 }
 
+function showDefault(){
+  
+    document.querySelector('#default').classList.remove('hidden')
+    document.querySelector('#result').innerText =""
+
+    document.querySelector('#playerPlaceholder').classList.toggle('hidden')
+    document.querySelector('#playerRock').classList.add('hidden')
+    document.querySelector('#playerPaper').classList.add('hidden')
+    document.querySelector('#playerScissor').classList.add('hidden')
+
+    document.querySelector('#computerPlaceholder').classList.toggle('hidden')
+    document.querySelector('#computerRock').classList.add('hidden')
+    document.querySelector('#computerPaper').classList.add('hidden')
+    document.querySelector('#computerScissor').classList.add('hidden')
+
+    rock.classList.remove('active')
+    paper.classList.remove('active')
+    scissor.classList.remove('active')
+    
+    gameOverCheck=false
+    
+}
+
+function again3(){
+    set3Active()
+    document.querySelector('#winner').classList.add('hidden')
+    document.querySelector('#loser').classList.add('hidden')
+    document.querySelector('#chooseToPlayAgain').classList.add('hidden')
+    showDefault()
+
+}
+
+function again5(){
+    set5Active()
+    document.querySelector('#winner').classList.add('hidden')
+    document.querySelector('#loser').classList.add('hidden')
+    document.querySelector('#chooseToPlayAgain').classList.add('hidden')
+    showDefault()
+}
+
+function again7(){
+    set7Active()
+    document.querySelector('#winner').classList.add('hidden')
+    document.querySelector('#loser').classList.add('hidden')
+    document.querySelector('#chooseToPlayAgain').classList.add('hidden')
+    showDefault()
+
+}
 /* Score */
 let playerScore=0;
 let computerScore=0;
+let savePlayerScore=0;
+let saveComputerScore=0;
+let gameOverCheck=false;
 
 document.querySelector('#playerScore').innerText = playerScore
 document.querySelector('#computerScore').innerText = computerScore
@@ -57,12 +130,30 @@ function gameOver(){
             document.querySelector('#winner').classList.remove('hidden')
             document.querySelector('#default').classList.add('hidden')
             document.querySelector('#loser').classList.add('hidden')
+            gameOverCheck=true;
+            savePlayerScore=playerScore
+    saveComputerScore=computerScore
+    playerScore=0
+    computerScore=0
+        
+            document.querySelector('#playerScore').innerText = playerScore
+document.querySelector('#computerScore').innerText = computerScore
+
         }
         else if (computerScore===2){
             document.querySelector('#winner').classList.add('hidden')
             document.querySelector('#default').classList.add('hidden')
             document.querySelector('#loser').classList.remove('hidden')
+            gameOverCheck=true;
+            savePlayerScore=playerScore
+    saveComputerScore=computerScore
+    playerScore=0
+    computerScore=0
+            document.querySelector('#playerScore').innerText = playerScore
+document.querySelector('#computerScore').innerText = computerScore
+
         }
+
     }
 
     if (gameTyp===5) {
@@ -70,11 +161,27 @@ function gameOver(){
             document.querySelector('#winner').classList.remove('hidden')
             document.querySelector('#default').classList.add('hidden')
             document.querySelector('#loser').classList.add('hidden')
+            gameOverCheck=true;
+            savePlayerScore=playerScore
+            saveComputerScore=computerScore
+            playerScore=0
+            computerScore=0
+            document.querySelector('#playerScore').innerText = playerScore
+document.querySelector('#computerScore').innerText = computerScore
+
         }
         else if (computerScore===3){
             document.querySelector('#winner').classList.add('hidden')
             document.querySelector('#default').classList.add('hidden')
             document.querySelector('#loser').classList.remove('hidden')
+            gameOverCheck=true;
+            savePlayerScore=playerScore
+            saveComputerScore=computerScore
+            playerScore=0
+            computerScore=0
+            document.querySelector('#playerScore').innerText = playerScore
+document.querySelector('#computerScore').innerText = computerScore
+
         }
     }
 
@@ -83,13 +190,54 @@ function gameOver(){
             document.querySelector('#winner').classList.remove('hidden')
             document.querySelector('#default').classList.add('hidden')
             document.querySelector('#loser').classList.add('hidden')
+            gameOverCheck=true;
+            savePlayerScore=playerScore
+            saveComputerScore=computerScore
+            playerScore=0
+            computerScore=0
+            document.querySelector('#playerScore').innerText = playerScore
+document.querySelector('#computerScore').innerText = computerScore
+
         }
         else if (computerScore===4){
             document.querySelector('#winner').classList.add('hidden')
             document.querySelector('#default').classList.add('hidden')
             document.querySelector('#loser').classList.remove('hidden')
+            gameOverCheck=true;
+            savePlayerScore=playerScore
+            saveComputerScore=computerScore
+            playerScore=0
+            computerScore=0
+            document.querySelector('#playerScore').innerText = playerScore
+document.querySelector('#computerScore').innerText = computerScore
+
         }
     }
+    
+}
+
+function showResult(){
+    if(gameOverCheck===true){
+        document.querySelector('#result').innerText = `The Final Score of the game was Player: ${savePlayerScore}  Computer: ${saveComputerScore}`
+    }
+}
+
+function askForRestart(){
+    if(gameOverCheck===true){
+        document.querySelector('#chooseHand').classList.add('hidden')
+        document.querySelector('#wannaRestart').classList.remove('hidden')
+        gameOverCheck===false;
+    }
+}
+
+function playAgain(){
+    document.querySelector('#wannaRestart').classList.add('hidden')
+    document.querySelector('#chooseToPlayAgain').classList.remove('hidden')
+}
+
+function thanksForPlaying(){
+    document.querySelector('#wannaRestart').classList.add('hidden')
+    document.querySelector('#thanksForPlaying').classList.remove('hidden')
 }
 
 /*Play a Hand*/
@@ -110,9 +258,14 @@ const computerPaper = document.querySelector('#computerPaper')
 const computerScissor = document.querySelector('#computerScissor')
 const computerPlaceholder = document.querySelector('#computerPlaceholder')
 
+
+// event Listener
 rock.addEventListener('click', setRock)
 paper.addEventListener('click', setPaper)
 scissor.addEventListener('click', setScissor)
+
+document.querySelector('#yesPlayAgain').addEventListener('click', playAgain)
+document.querySelector('#noPlayAgain').addEventListener('click', thanksForPlaying)
 
 function setRock() {
     rock.classList.remove('active')
@@ -131,7 +284,11 @@ function setRock() {
     checkGame()
     updateScore()
     gameOver()
-   
+    showResult()
+    askForRestart()
+    
+    
+    
     
 
 }
@@ -154,6 +311,8 @@ function setPaper() {
     checkGame()
     updateScore()
     gameOver()
+    showResult()
+    askForRestart()
     
 
 
@@ -177,7 +336,8 @@ function setScissor() {
     checkGame()
     updateScore()
     gameOver()
-    
+    showResult()
+    askForRestart()
 }
 
 
